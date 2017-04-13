c54f39d1ecf7362acc34f4780d1bac1d9fa5a649
Tinsson <695724046@qq.com>
2017-04-13 17:14:28 +0800

    about database

--- database/timetrack_server.js
+++ database/timetrack_server.js
@@ -6,15 +6,52 @@ var db = mysql.createConnection({
     user: 'root',
     password: '123456',
     database: 'mydb'
-})
+});
+var server = http.createServer(function(req,res){
+    switch (req.method){
+        case 'POST':
+            switch(req.url){
+                case '/':
+                    work.add(db,req,res);
+                    break; 
+                case '/archive':
+                    work.archive(db,req,res);
+                    break;
+                case '/delete':
+                    work.delete(db,req,res);
+                    break;
+            }
+            break;
+        case 'GET':
+            switch(req.url){
+                case '/':
+                    work.show(db,res);
+                    break;
+                case '/archived':
+                    work.showArchived(db,res);
+            }
+            break;
+    }
+});
 db.connect(function(err){
     if(err) throw err;
-    console.log("connect as id " + db.threadId);
 });
 // var sql = "select * from myguests where id = " + db.escape(2);
-db.query("select * from myguests where id = ?",["2"],function(err,rows,fields){
+// db.query("select * from myguests where id = ?",["2"],function(err,rows,fields){
+//     if(err) throw err;
+//     console.log(rows[0].firstname);
+//     console.log(rows.length);
+// })
+var sql = 'CREATE TABLE IF NOT EXISTS word (';
+    sql += 'id INT(10) NOT NULL AUTO_INCREMENT';
+    sql += 'hours DECIMAL(5,2) DEFAULT 0,';
+    sql += 'data DATE,';
+    sql += 'archived INT(1) DEFAULT 0,';
+    sql += 'description LONGTEXT';
+    sql += 'PRIMARY KEY(id)';
+db.query(sql,function(err){
     if(err) throw err;
-    console.log(rows[0].firstname);
-    console.log(rows.length);
+    console.log('Server started...');
+    server.listen(3000);
 })
 db.end();
